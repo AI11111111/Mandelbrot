1847ebbf86298259dc0fc8c6cbaa5b0a210b420e
alessandra <w_alessandra@hotmail.com>
2025-07-07 00:36:07 +0200

    deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppNavigation from "@/components/AppNavigation.vue";
 import Videos from '@/views/Videos.vue'
+import Mandelbrot from '@/views/Mandelbrot.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: AppNavigation,
+      component: Mandelbrot,
     },
     {
       path: '/about',
