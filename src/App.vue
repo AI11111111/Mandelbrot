08259df4363c130b4e0d2d10d30fff6ae8d43499
alessandra <w_alessandra@hotmail.com>
2025-07-07 01:26:15 +0200

    deploy

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
           <div class="flex space-x-6 text-sm font-medium">
             <RouterLink
               to="/grafik"
-              class="transition duration-200 px-3 py-2 rounded bg-blue-100 hover:text-blue-600"
-              :class="{ 'bg-blue-100 text-white': $route.path === '/grafik' }"
+              class="transition duration-200 px-3 py-2 rounded  hover:text-blue-600"
+              :class="{ 'text-white': $route.path === '/grafik' }"
             >
                 <ImageIcon class="inline w-4 h-4 mr-1" /> Grafik
             </RouterLink>
