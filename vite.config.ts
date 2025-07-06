0b6c20c8366df72cd0c9aaafbb9dba084a17b876
alessandra <w_alessandra@hotmail.com>
2025-07-06 23:31:38 +0200

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
-    ghPages()
   ],
   base: '/Mandelbrot/',
   build: {
