557138a7a56ba95eff05a11f842ac962fc853fe8
alessandra <w_alessandra@hotmail.com>
2025-07-07 01:27:08 +0200

    deploy

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <nav class="bg-white shadow sticky top-0 z-50">
       <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 items-center">
-          <h1 class="text-2xl font-bold hsla(160, 100%, 37%, 1)">🎨 Mandelbrot</h1>
+          <h1 class="text-2xl font-bold hsla(160, 100%, 37%, 1)">Mandelbrot</h1>
           <div class="flex space-x-6 text-sm font-medium">
             <RouterLink
               to="/grafik"
@@ -16,7 +16,7 @@
             <RouterLink
               to="/videos"
               class="transition duration-200 px-3 py-2 rounded hover:bg-blue-100 hover:text-blue-600"
-              :class="{ 'bg-blue-100 text-white': $route.path === '/videos' }"
+              :class="{ 'text-white': $route.path === '/videos' }"
             >
               <Video class="inline w-4 h-4 mr-1" />  Videos
             </RouterLink>
