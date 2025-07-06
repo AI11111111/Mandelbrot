3435558e07b0a4b17f12431cd42303f493db75bd
alessandra <w_alessandra@hotmail.com>
2025-07-07 00:37:20 +0200

    deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,7 +18,6 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../components/AppNavigation.vue'),
     },
   ],
 })
