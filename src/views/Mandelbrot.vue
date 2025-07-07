0b6ea0edbba388d474aa9e4535433fe12a77bbb9
alessandra <w_alessandra@hotmail.com>
2025-07-07 22:06:23 +0200

    deploy

--- src/views/Mandelbrot.vue
+++ src/views/Mandelbrot.vue
@@ -45,7 +45,7 @@
         <!-- Toggle Button -->
         <button
           @click="toggleAnimation"
-          class="mt-6 w-full px-4 py-2 rounded text-white"
+          class="my-6 w-full px-4 py-2 rounded text-white"
           :class="isAnimating ? 'bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600'"
         >
           {{ isAnimating ? 'Stop' : 'Start' }}
